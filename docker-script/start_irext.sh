4f3b67be7f296f7a6865f75e1a96b9271028b9cc
strawmanbobi <strawmanbobi@163.com>
2025-10-29 13:31:27 +0800

    updated private server docker script

--- docker-script/start_irext.sh
+++ docker-script/start_irext.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
+WORKSPACE=$(dirname "$(realpath '${0}')")
 source /etc/profile
 chown mysql:mysql /data/mysql -R
 chmod 755 /data/mysql -R
 service mysql restart
-sleep 5
 service redis-server restart
 sleep 5
-nohup java -jar /data/irext/private-cloud/server/package/private-server-0.2.5.jar > log.out 2>&1 &
-echo "private server started"
+
+echo "Stopping private-backend"
+pkill java
+sleep 2
+
+echo "Starting private-backend"
+nohup java -jar /data/irext/private-cloud/private-backend/package/private-backend-1.5.0.jar > log.out 2>&1 &
 sleep 5
-cd /data/irext/private-cloud/console
+
+echo "Stopping private-console"
+cd /data/irext/private-cloud/private-console
+pkill node
+
+echo "Starting private-console"
+sleep 2
 ./startup.sh
-echo "private console started"
+
+cd ${WORKSPACE}
+
+echo "IRext private server started"
 /bin/bash
 
